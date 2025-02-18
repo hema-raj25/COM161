c72bc61259594b22f82af41fa25606e234dd9ab0
hema-raj25 <hemalatha.raj@qa.com>
2025-02-18 16:41:32 +0000

    Add files via upload

---
+++ square.py
@@ -0,0 +1,19 @@
+# this program draws a square of side 100
+
+#import turtle
+import turtle
+
+# draw a line of length 100
+# then turn left thru 90 degrees
+# repeat this process 4 times to
+# draw the four sides of the square
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
